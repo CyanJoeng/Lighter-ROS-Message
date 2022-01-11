6ffcff15f2ed97af1cbc91c7d789c1fde11b9a39
yanglichen <yanglichen@baidu.com>
2022-01-11 16:13:20 +0800

    Feat: add opencv viz to test

--- test/pubsub_odometry.cpp
+++ test/pubsub_odometry.cpp
@@ -5,14 +5,17 @@
 #include <cstdio>
 #include <cstdlib>
 #include <list>
+#include <memory>
 #include <mutex>
 #include <opencv2/core/types.hpp>
+#include <opencv2/viz/widgets.hpp>
 #include <string>
 #include <thread>
 
 #include <boost/program_options.hpp>
 
 #include <opencv2/opencv.hpp>
+#include <opencv2/viz.hpp>
 
 #include "cmg/cmg.hpp"
 #include "messages/nav_msgs/Odometry.hpp"
@@ -20,81 +23,45 @@
 using namespace cmg;
 namespace po = boost::program_options;
 
+using Widget = cv::viz::WPolyLine;
+using WidgetPtr = std::shared_ptr<Widget>;
+
 static std::string img_path;
+static constexpr auto name_odo = "odometry";
 
-auto draw_odo(const cmg::nav_msgs::OdometryConstPtr &odometry) -> cv::Mat {
 
-	static auto bg_img = cv::imread(img_path, cv::IMREAD_ANYCOLOR);
-
-	cv::Mat img = bg_img.clone();
-	if (img.empty())
-		img = cv::Mat::zeros(500, 500, CV_8UC3);
-
-	cv::putText(img, "stamp: " + std::to_string(odometry->header.stamp.toSec()), cv::Point(0, 10), 0, .5, cv::Scalar {255, 255, 0}, 1);
-
-	static cv::Point2d max_xy;
-
-	static float w_2 = img.cols * .5f;
-	static float h_2 = img.rows * .5f;
-
-	auto &position = odometry->pose.position;
-	max_xy.x = std::max(std::fabs(position.x * 10), max_xy.x);
-	max_xy.y = std::max(std::fabs(position.y * 10), max_xy.y);
-
-	static float scale = 1;
-
-	float scale_x = max_xy.x > w_2 ? max_xy.x / w_2: 1;
-	float scale_y = max_xy.y > h_2 ? max_xy.y / h_2: 1;
-
-	scale = std::max(scale_x, scale_y);
-
-	auto odo_to_point = [w_2=img.cols/2, h_2=img.rows/2](auto x, auto y) -> cv::Point {
-
-		int off_x = w_2 + (x * 10) / scale + .5f;
-		int off_y = h_2 + (y * 10) / scale + .5f;
-
-		return {off_x, off_y};
-	};
-
-	static std::list<cmg::nav_msgs::OdometryConstPtr> odos;
-
-	odos.push_back(odometry);
-
-	cv::Point last_p = odo_to_point(odos.front()->pose.position.x * 10, odos.front()->pose.position.y * 10);
-
-	for (auto odo : odos) {
-
-		auto pt = odo_to_point(odo->pose.position.x, odo->pose.position.y);
-		cv::line(img, last_p, pt, cv::Scalar {255, 255, 0}, 1);
-
-		//std::cout << last_p << pt << std::endl;
-		last_p = pt;
-	}
-
-	return img;
-}
-
-auto create_odo(int i) -> cmg::nav_msgs::Odometry {
+auto create_odo(int id) -> cmg::nav_msgs::Odometry {
 
 	return {};
 }
 
-static cv::Mat show_img;
-static std::mutex img_mt;
+auto draw_odo_3d(const cmg::nav_msgs::OdometryConstPtr &odometry) -> WidgetPtr {
+
+	static std::vector<cv::Point3d> hist_points;
+	hist_points.emplace_back(odometry->pose.position.x, odometry->pose.position.y, odometry->pose.position.z);
+
+	auto polyline = std::make_shared<Widget>(hist_points);
+
+	return polyline;
+}
+
+static std::optional<cv::viz::Viz3d> window;
+static WidgetPtr widget_odo;
+static std::mutex widget_mt;
 static std::thread work_loop;
 
 void ui_refresh() {
 
 	while (true) {
 
-		if (!show_img.empty()) {
+		if (widget_odo) {
 
 			{
-				std::lock_guard<std::mutex> lock(img_mt);
+				std::lock_guard<std::mutex> lock(widget_mt);
 
-				cv::imshow("callback", show_img);
+				window->showWidget(name_odo, *widget_odo);
 			}
-			cv::waitKey(10);
+			window->spinOnce();
 		}
 
 		std::this_thread::sleep_for(std::chrono::duration<double>(.1));
@@ -109,10 +76,10 @@ auto cb(const cmg::nav_msgs::OdometryConstPtr &odometry) {
 	printf("cb odometry stamp %f (%10.4f %10.4f %10.4f)\n", odometry->header.stamp.toSec(),
 			position.x, position.y, position.z);
 
-	auto img = draw_odo(odometry);
+	auto odo = draw_odo_3d(odometry);
 	{
-		std::lock_guard<std::mutex> lock(img_mt);
-		img.copyTo(show_img);
+		std::lock_guard<std::mutex> lock(widget_mt);
+		widget_odo.swap(odo);
 	}
 }
 
@@ -125,7 +92,8 @@ auto args_parser(int argc, char *argv[]) -> po::variables_map {
 		("proc", po::value<std::string>()->default_value("server"), "server proc name")
 		("topic", po::value<std::string>()->default_value("foo"), "topic name")
 		("cfg", po::value<std::string>()->default_value(""), "config file")
-		("bg_image", po::value<std::string>()->default_value(""), "background image of canvas");
+		("bg_image", po::value<std::string>()->default_value(""), "background image of canvas")
+		("gl", po::value<bool>()->default_value(true), "enable opengl window");
 
 	po::positional_options_description pos_desc;
 	pos_desc.add("mode", 1);
@@ -198,13 +166,12 @@ int main(int argc, char *argv[]) {
 
 		std::string proc_topic = "/" + proc + "/" + topic;
 
-		work_loop = std::thread {
-			[&]() {
 
-				auto sub = n.subscribe(proc_topic, 1000, cb);
-				cmg::spin();
-			}
-		};
+		work_loop = std::thread {[&]() {
+
+			auto sub = n.subscribe(proc_topic, 1000, cb);
+			cmg::spin();
+		}};
 		ui_refresh();
 	}
 
